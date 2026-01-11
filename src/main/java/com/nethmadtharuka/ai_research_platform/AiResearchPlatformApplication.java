35010f5835ca0d305896fcbf62bf936ad6c45f5c
nethmadtharuka <nethmaxx12345@gmail.com>
2026-01-11 20:03:26 +0530

    Initial commit

---
+++ src/main/java/com/nethmadtharuka/ai_research_platform/AiResearchPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.nethmadtharuka.ai_research_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiResearchPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiResearchPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nethmadtharuka/ai_research_platform/AiResearchPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nethmadtharuka.ai_research_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiResearchPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
